6b64c60174e540ea35019ba9769760ce1c061841
Kaisar Arkhan <yukinagato@protonmail.com>
2016-06-02 15:18:08 +0700

    scene: Added Documentation

--- scene/scene.go
+++ scene/scene.go
@@ -1,9 +1,14 @@
+// Scene provides an interface for scenes
 package scene
 
 import "github.com/yukiisbored/Kaori/common"
 
+// Scene is an interface the provides the needed functions for a usual scene
 type Scene interface {
+	// Enter executes when the scene is going to be used
 	Enter()
+
+	// Exit executes when the scene is going to be replaced or stopped
 	Exit()
 
 	common.Updater
