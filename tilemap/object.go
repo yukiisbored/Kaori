f79ec8b515236730583fbb7083d7732802b08807
Kaisar Arkhan <yukinagato@protonmail.com>
2016-06-03 12:45:54 +0700

    tiledmap: fixed golint problems for object

--- tilemap/object.go
+++ tilemap/object.go
@@ -1,10 +1,13 @@
 package tilemap
 
+// ObjectGroup is a group of tiled map objects
 type ObjectGroup struct {
 	Name    string    `xml:"name,attr"`
 	Objects []*Object `xml:"object"`
 }
 
+// Object is a tile map object
+// It can be an ellipse, rectangle, or a polyline
 type Object struct {
 	Id int `xml:"id,attr"`
 
@@ -19,6 +22,7 @@ type Object struct {
 	Polyline Polyline `xml:"polyline"`
 }
 
+// Polyline is a list of points representing a polygonal shape or line
 type Polyline struct {
 	Points string `xml:"points,attr"`
 }
